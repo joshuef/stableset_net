b5991c8d51352e220898af3d729b7b1eef57fa4a
qima <qi.ma@maidsafe.net>
2023-05-26 00:49:11 +0800

    chore: extend REPLICATION_RANGE to 3

--- safenode/src/network/event.rs
+++ safenode/src/network/event.rs
@@ -40,7 +40,7 @@ use tracing::{info, warn};
 // Defines how close that a node will trigger repliation.
 // That is, the node has to be among the REPLICATION_RANGE closest to data,
 // to carry out the replication.
-const REPLICATION_RANGE: usize = 2;
+const REPLICATION_RANGE: usize = 3;
 
 #[derive(NetworkBehaviour)]
 #[behaviour(out_event = "NodeEvent")]
