a0bd37cfbe09595d0fb5f1ed2a578f2341c02dd8
Josh Wilson <joshuef@gmail.com>
2023-06-23 10:44:19 +0900

    log addresses of newly added peers

--- sn_networking/src/event.rs
+++ sn_networking/src/event.rs
@@ -393,9 +393,13 @@ impl SwarmDriver {
                 // TODO: send an error response back?
             }
             KademliaEvent::RoutingUpdated {
-                peer, is_new_peer, ..
+                peer,
+                is_new_peer,
+                addresses,
+                ..
             } => {
                 if is_new_peer {
+                    info!("Addresses for new peer: {addresses:?}");
                     self.log_kbucket(&peer);
                     self.event_sender
                         .send(NetworkEvent::PeerAdded(peer))
